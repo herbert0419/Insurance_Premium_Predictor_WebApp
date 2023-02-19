479f11f44f7bb9c28041f5e16b35803e0c72d5ad
Shobhandeb Paul <55660915+herbert0419@users.noreply.github.com>
2023-02-19 21:28:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,92 +1,65 @@
-# import yfinance as yf
-# import numpy as np
-# from sklearn.linear_model import LinearRegression
-
-# # Load historical data for ADANIENT.NS from Yahoo Finance
-# ticker = "ADANIENT.NS"
-# df = yf.download(ticker, start="2020-01-01", end="2022-02-18")
-
-# # Create features and target variables
-# df["SMA_20"] = df["Close"].rolling(window=20).mean()
-# df["SMA_50"] = df["Close"].rolling(window=50).mean()
-# df["SMA_200"] = df["Close"].rolling(window=200).mean()
-# df["Return"] = df["Close"].pct_change()
-# df.dropna(inplace=True)
-
-# X = df[["SMA_20", "SMA_50", "SMA_200", "Return"]].values
-# y = df["Close"].values
-
-# # Split data into training and testing sets
-# n = len(df)
-# train_X, train_y = X[:int(n * 0.8)], y[:int(n * 0.8)]
-# test_X, test_y = X[int(n * 0.8):], y[int(n * 0.8):]
-
-# # Train linear regression model
-# model = LinearRegression()
-# model.fit(train_X, train_y)
-
-# # Make predictions on test set
-# predictions = model.predict(test_X)
-
-# # Calculate mean absolute error (MAE) on test set
-# mae = np.mean(np.abs(predictions - test_y))
-# print("Mean absolute error:", mae)
-
-
-import yfinance as yf
-import pandas as pd
+import streamlit as st
+# import preprocessor,helper
+import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.preprocessing import MinMaxScaler
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, LSTM
+import pandas as pd
+import seaborn as sns
+import pickle
+import xgboost as xg
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
 
-# Download data
-df = yf.download('ADANIENT.NS', start='2010-01-01', end='2023-02-19')
+model = pickle.load(open('model_final.pkl','rb'))
+encoder = pickle.load(open('target_encoder.pkl','rb'))
+transformer = pickle.load(open('transformer.pkl','rb'))
 
-# Prepare data
-data = df.filter(['Close'])
-dataset = data.values
-training_data_len = int(np.ceil(len(dataset) * 0.8))
-scaler = MinMaxScaler(feature_range=(0, 1))
-scaled_data = scaler.fit_transform(dataset)
+st.title("Insurance Predictor")
+age = st.text_input('Enter Age', 18)
+age = int(age)
 
-train_data = scaled_data[0:training_data_len, :]
-x_train = []
-y_train = []
+sex = st.selectbox(
+    'Please select gender',
+    ('male', 'female'))
+# gender = encoder.transform(np.array([sex]))
 
-for i in range(60, len(train_data)):
-    x_train.append(train_data[i-60:i, 0])
-    y_train.append(train_data[i, 0])
+bmi = st.text_input('Enter BMI', 18)
+bmi = float(bmi)
 
-x_train, y_train = np.array(x_train), np.array(y_train)
-x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+children = st.selectbox(
+    'Please select number of children ',
+    (0,1,2,3,4,5))
+children = int(children)
 
-# Build model
-model = Sequential()
-model.add(LSTM(50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
-model.add(LSTM(50, return_sequences=False))
-model.add(Dense(25))
-model.add(Dense(1))
-model.compile(optimizer='adam', loss='mean_squared_error')
 
-# Train model
-model.fit(x_train, y_train, batch_size=1, epochs=1)
+smoker = st.selectbox(
+    'Please select smoker category ',
+    ("yes","no"))
+# smoker = encoder.transform(smoker)
 
-# Test model
-test_data = scaled_data[training_data_len-60:, :]
-x_test = []
-y_test = dataset[training_data_len:, :]
-for i in range(60, len(test_data)):
-    x_test.append(test_data[i-60:i, 0])
+region = st.selectbox(
+    'Please select region ',
+    ("southwest", "southeast", "northeast", "northwest"))
 
-x_test = np.array(x_test)
-x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 
-predictions = model.predict(x_test)
-predictions = scaler.inverse_transform(predictions)
+l = {}
+l['age'] = age
+l['sex'] = sex
+l['bmi'] = bmi
+l['children'] = children
+l['smoker'] = smoker
+l['region'] = region
 
-# Visualize predictions
-train = data[:training_data_len]
-valid = data[training_data_len:]
-valid['Predictions'] = predictions
+df = pd.DataFrame(l, index=[0])
 
+df['region'] = encoder.transform(df['region'])
+df['sex'] = df['sex'].map({'male':1, 'female':0})
+df['smoker'] = df['smoker'].map({'yes':1, 'no':0})
+
+df = transformer.transform(df)
+# dtrain = xg.DMatrix(df)
+y_pred = model.predict(df)
+# st.write(age, gender, bmi, children, smoker, region)
+
+if st.button("Show Result"):
+    # col1,col2, col3,col4 = st.columns(4)
+    st.header(f"{round(y_pred[0],2)} INR")
